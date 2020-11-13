31a30d69a7fb1f55b05738d8efc254e24e878b09
Crystal Hwang <crystal.hwang007@gmail.com>
2020-11-13 10:22:27 +0900

    [Feat] : Create Routes
    I created routes like root, login, rooms and  groups in AppContainer.js.

--- src/containers/AppContainer/AppContainer.js
+++ src/containers/AppContainer/AppContainer.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Home from '../../components/Home/Home';
+import Login from '../../components/Login/Login';
+import Room from '../../components/Room/Room';
+import Group from '../../components/group/group';
 
 import styles from './AppContainer.module.css';
 
-function AppContainer({
-}) {
+function AppContainer({ }) {
+  const [isLoggedIn, setISLoggedIn] = useState(false);
 
   return (
     <div className={styles.AppContainer}>
+      <Switch>
+        <Route exact path='/'>
+          {
+            isLoggedIn
+              ? <Room />
+              : <Home />
+          }
+        </Route>
+        <Route path='/login'>
+          {
+            isLoggedIn
+              ? <Room />
+              : <Login />
+          }
+        </Route>
+        <Route path='/rooms'>
+          <Room />
+        </Route>
+        <Route path='/groups'>
+          <Group />
+        </Route>
+      </Switch>
     </div>
   );
 };
