6dcebeebe548547d3b6488cee8086ba06e8233e8
Crystal Hwang <crystal.hwang007@gmail.com>
2020-11-13 19:15:23 +0900

    [Fix] : Change way to  request to server from axios to fetch

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,29 +1,34 @@
-import axios from 'axios';
-
 import { authService, googleProvider } from '../config/firebase';
 import { API_METHOD } from '../constants';
 
 export const signInWithGoogle = () => {
   googleProvider.addScope('email');
 
-  return authService.signInWithPopup(googleProvider)
+  authService.signInWithPopup(googleProvider)
     .then(async (userInfo) => {
+      console.log("GOOGLE", userInfo);
       const { POST } = API_METHOD;
       const postData = {
         email: userInfo.additionalUserInfo.profile.email,
-        nickname: userInfo.additionalUserInfo.profile.userName,
+        nickname: userInfo.additionalUserInfo.profile.name,
       };
 
-      const authorizedUserWithToken = await axios.post({
+      fetch('http://localhost:5000/login', {
         method: POST,
-        url: 'http://localhost:5000/login',
-        data: postData
-      }).then((response) => {
-        console.log("token?", response);
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      }).then((result) => {
+        console.log("LOGIN 된 후에 돌아온 데이터", result);
+        console.log("그 데이터의 body", result.body);
+        return result.json();
+      }).then((data) => {
+        console.log('제발', data);
+      }).catch((err) => {
+        console.error(err);
       });
-
-      return authorizedUserWithToken;
-    }).catch((error) => {
-      console.error(error);
+    }).catch((err) => {
+      console.error(err);
     });
 };
