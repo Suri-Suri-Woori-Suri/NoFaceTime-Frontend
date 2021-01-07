4e499c3b470eee1299a1b9424234241099816598
Crystal Hwang <crystal.hwang007@gmail.com>
2021-01-07 13:08:28 +0900

    [Feat] : set SERVER_URL to change according to execution environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,9 @@
-//export const SERVER_URL = 'https://api.twofacetime.xyz';
-export const SERVER_URL = 'https://localhost:5000';
+let SERVER_URL;
+
+if (process.env.NODE_ENV === 'development') {
+  SERVER_URL = 'https://localhost:5000';
+} else {
+  SERVER_URL = 'https://api.twofacetime.xyz';
+}
+
+export default SERVER_URL;
